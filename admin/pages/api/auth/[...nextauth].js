ecdadc188f42dd544ddc2e7389bc5593bb324224
Yassine <yachour92@gmail.com>
2024-10-17 16:54:25 +0200

    ci: ajouter l'auth et mongoDB

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -1,19 +1,19 @@
+import clientPromise from '@/lib/db';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import NextAuth, {getServerSession} from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-// import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
-// import clientPromise from "@/lib/mongodb";
 
 const adminEmails = ['yachour92@gmail.com'];
 
 export const authOptions = {
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
   ],
-  // adapter: MongoDBAdapter(clientPromise),
+  adapter: MongoDBAdapter(clientPromise),
   // callbacks: {
   //   session: ({session,token,user}) => {
   //     if (adminEmails.includes(session?.user?.email)) {
--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "./components/Nav";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -6,7 +7,10 @@ export default function Home() {
     return (
       <div className="bg-blue-900 w-screen h-screen flex items-center">
         <div className="text-center w-full">
-          <button onClick={()=>signIn('google')} className="bg-white p-2 px-4 rounded-lg">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white p-2 px-4 rounded-lg"
+          >
             Login with google
           </button>
         </div>
@@ -14,5 +18,10 @@ export default function Home() {
     );
   }
 
-  return <div>logged in {session.user.email}</div>;
+  return (
+    <div className="bg-blue-900 min-h-screen flex">
+      <Nav />
+      <div className="bg-white flew-grew mt-1 mr-2 mb-2 rounded-lg p-4">logged in {session.user.email}</div>
+    </div>
+  );
 }
